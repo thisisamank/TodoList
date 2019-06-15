7df1eadf8530f04fb2c15b99513565e9e69ea767
Aman <thisisamank@gmail.com>
2019-06-15 16:08:18 +0530

    Updates

---
+++ precache-manifest.fb2d261874cd498290141b1fd54d2509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcebf22ba2a9928df445cdcf064b64e6",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "98b71559335f016190e5",
+    "url": "/todolist/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6264f0ebae122fd0aef3",
+    "url": "/todolist/static/css/main.35f76285.chunk.css"
+  },
+  {
+    "revision": "98b71559335f016190e5",
+    "url": "/todolist/static/js/2.443f08ca.chunk.js"
+  },
+  {
+    "revision": "6264f0ebae122fd0aef3",
+    "url": "/todolist/static/js/main.ea2673a1.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
